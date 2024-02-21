716ead0174ed3d13c5f7cf26be663972859f15f7
Vanagandr111 <135647718+Vanagandr111@users.noreply.github.com>
2024-02-21 13:42:18 +0700

    Update XmlToJsonParser.java

--- src/main/java/org/example/XmlToJsonParser.java
+++ src/main/java/org/example/XmlToJsonParser.java
@@ -1,4 +1,38 @@
 package org.example;
 
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class XmlToJsonParser {
+
+    public static List<Employee> parseXML(String filename) {
+        List<Employee> employees = new ArrayList<>();
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(new File(filename));
+            document.getDocumentElement().normalize();
+            NodeList nodeList = document.getElementsByTagName("employee");
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+                    Employee employee = new Employee();
+                    employee.setId(Long.parseLong(element.getElementsByTagName("id").item(0).getTextContent()));
+                    employee.setFirstName(element.getElementsByTagName("firstName").item(0).getTextContent());
+                    employee.setLastName(element.getElementsByTagName("lastName").item(0).getTextContent());
+                    employee.setCountry(element.getElementsByTagName("country").item(0).getTextContent());
+                    employee.setAge(Integer.parseInt(element.getElementsByTagName("age").item(0).getTextContent()));
+                    employees.add(employee);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return employees;
+    }
 }
